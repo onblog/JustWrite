51a318285d001a3f5df2cf523c1e064506d055dc
onblog <codeblog@qq.com>
2019-11-11 19:04:53 -0600

    Update index.js

--- src/index.js
+++ src/index.js
@@ -488,43 +488,6 @@ function uploadPictureToWeiBo(filePath) {
         if (err) {
             return console.error(err)
         }
-        const image_data = 'b64_data=' + urlEncode(data)
-        let options = {
-            host: 'picupload.weibo.com',
-            method: 'POST',
-            headers: {
-                'Accept-Encoding': 'gzip, deflate, br',
-                "Accept-Language": "zh-CN,zh;q=0.8,en-US;q=0.5,en;q=0.3",
-                'Referer': 'https://weibo.com/',
-                'Accept': '*/*',
-                'Origin': 'https://weibo.com',
-                'Content-Type': 'application/x-www-form-urlencoded',
-                'Content-Length': Buffer.byteLength(image_data),
-                'User-Agent': 'Mozilla/5.0 (Windows NT 6.1; WOW64; rv:45.0) Gecko/20100101 Firefox/45.0',
-                'Cookie': dataStore.getWeiBoCookies()
-            }
-        }
-
-        let req = https.request(image_url, options, function (res) {
-            // console.log('STATUS: ' + res.statusCode);
-            res.setEncoding('utf8');
-            const prefix = 'http://ww3.sinaimg.cn/large/'
-            res.on('data', function (chunk) {
-                // console.log('BODY: ' + chunk);
-                let start = chunk.toString().indexOf('script>') + 'script>'.length
-                const text = chunk.toString().substring(start).trim()
-                // console.log(text)
-                const parse = JSON.parse(text)
-                const pid = parse.data.pics.pic_1.pid
-                const src = prefix + pid
-                // console.log(src)
-                if (pid === undefined || pid === null) {
-                    alert('请先设置新浪微博图床')
-                } else {
-                    insertPictureToTextarea(tab, src)
-                }
-            });
-        });
 
         req.on('error', function (e) {
             console.log('problem with request: ' + e.message);
