80f58b67ec5b32e1542a88f11ab72290c26d0069
onblog <codeblog@qq.com>
2019-12-14 18:22:54 +0800

    win窗口图标问题解决了，在新建窗口的时候指定'./build/app.png'即可

--- main.js
+++ main.js
@@ -16,6 +16,7 @@ function createWindow() {
     mainWindow = new BrowserWindow({
                                        width: 800,
                                        height: 600,
+                                       icon: './build/app.png',
                                        webPreferences: {devTools: false, nodeIntegration: true},
                                        titleBarStyle: "hidden"
                                    })
